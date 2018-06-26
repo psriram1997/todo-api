f5af17e9a0b1ca734d83013a0ffce49802653e4f
sriram perla <psriram1997@gmail.com>
2018-06-27 01:06:30 +0530

    todo routes

--- playground/mongo_delete.js
+++ playground/mongo_delete.js
@@ -1,33 +1,39 @@
 const {MongoClient, ObjectID} = require('mongodb');
 
+var id = new ObjectID();
+console.log((id));
 
-MongoClient.connect('mongodb://localhost:27017',(err, client) => {
-   if(err){
-       return console.log('Unable to connect to Datatbase server');
-   }
-   console.log('Connected to MongoDB');
-    var db = client.db('TodoApp');
-    //deleteMany
-    db.collection('Todos').deleteMany({completed:true}).then((result) => {
-        console.log(result)
-    });
-    db.collection('Users').deleteMany({name:'Sriram'}).then((result) => {
-        console.log(result)
-    });
-    //deleteOne
-    db.collection('Todos').deleteOne({completed:true}).then((result) => {
-        console.log(result)
-    });
-    db.collection('Users').deleteOne({name:'Sriram'}).then((result) => {
-        console.log(result)
-    });
-    //findOneAndDelete
-    db.collection('Todos').findOneAndDelete({completed:true}).then((result) => {
-        console.log(result.value)
-    });
-    //---------------using _id finding user and deleting-----------------
-    db.collection('Users').findOneAndDelete({_id: new ObjectID("5b2dd538e333c225487ad7a4")}).then((result) => {
-        console.log(result.value)
-    });
-    client.close();
-});
+console.log(ObjectID.isValid("5b3231a0883d571d0c1f857b"));
+
+""
+
+// MongoClient.connect('mongodb://localhost:27017',(err, client) => {
+//    if(err){
+//        return console.log('Unable to connect to Datatbase server');
+//    }
+//    console.log('Connected to MongoDB');
+//     var db = client.db('TodoApp');
+//     //deleteMany
+//     db.collection('Todos').deleteMany({completed:true}).then((result) => {
+//         console.log(result)
+//     });
+//     db.collection('Users').deleteMany({name:'Sriram'}).then((result) => {
+//         console.log(result)
+//     });
+//     //deleteOne
+//     db.collection('Todos').deleteOne({completed:true}).then((result) => {
+//         console.log(result)
+//     });
+//     db.collection('Users').deleteOne({name:'Sriram'}).then((result) => {
+//         console.log(result)
+//     });
+//     //findOneAndDelete
+//     db.collection('Todos').findOneAndDelete({completed:true}).then((result) => {
+//         console.log(result.value)
+//     });
+//     //---------------using _id finding user and deleting-----------------
+//     db.collection('Users').findOneAndDelete({_id: new ObjectID("5b2dd538e333c225487ad7a4")}).then((result) => {
+//         console.log(result.value)
+//     });
+//     client.close();
+// });
--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 const express    = require('express');
 const app        = express();
 const bodyParser = require('body-parser');
+const {ObjectId} = require('mongodb');
+const _          = require('lodash');
 
 const {mongoose} = require('./database/db_connection');
 const {Todo}     = require('./models/todo');
 const {User}     = require('./models/user');
 
 app.use(bodyParser.json());
-
+//----------Todo routes------------
+//-------POST route-------------
 app.post('/todos', (req, res) => {
     var newTodo = new Todo({
         text : req.body.text
@@ -19,7 +22,79 @@ app.post('/todos', (req, res) => {
         res.status(400).send(err);
     });
 });
+//-------------GET TODOS ------------
+app.get("/todos", (req, res) => {
+    
+    Todo.find({}).then((todos) => {
+        res.send({todos});
+    },(e) => {
+        res.status(400).send(e);
+    });
+});
+//-------GET INDIVIDUAL TODO---------
+app.get("/todos/:id", (req, res) => {
+    var id = req.params.id;
+    if(!ObjectId.isValid(id)){
+        return res.status(400).send();
+    }
+    
+    Todo.findById(id).then((todo) => {
+        if(todo){
+            res.send({todo});
+        }else{
+            res.status(404).send();
+        }
+        
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+//-----Delete Route-----------
+app.delete("/todos/:id", (req, res) => {
+    var id = req.params.id;
+    if(!ObjectId.isValid(id)){
+        return res.status(400).send();
+    }
 
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if(todo){
+            res.send({todo});
+        }
+        else{
+            res.status(404).send();
+        }
+    },(err) => {
+        res.status(400).send();
+    });
+});
+//----Update Route--------------
+app.patch("/todos/:id", (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+    if(body.completed){
+        body.completedAt = new Date().getTime();
+    }else{
+        body.completedAt = null;
+    }
+    
+    if(!ObjectId.isValid(id)){
+        return res.status(400).send();
+    }
+
+    Todo.findByIdAndUpdate(id, body, {new: true}).then((todo) => {
+        res.send({todo})
+    },(err) => {
+        res.status(400).send(err);
+    });
+
+});
+
+
+
+
+
+
+//------Users Routes-------------
 app.post('/user', (req, res) => {
     var newUser = new User({
         email : req.body.email
